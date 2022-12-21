5f64ebdfd993e2df853ee0c5b10df68536f2e776
Ishaan <ishaanrejra@gmail.com>
2022-12-21 11:59:42 +0530

    Create square-pattern.cpp

---
+++ Pattern-questions/square-pattern.cpp
@@ -0,0 +1,28 @@
+/* 
+Print the following pattern:
+for n= 4
+The output is:
+4444
+4444
+4444
+4444
+ */
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+  int n;
+  cout<<"Enter the value of n:";
+  cin>>n;
+  cout <<"The pattern is :"<<endl;
+  for(int i = 0;i<n;i++)
+  {
+    for(int j = 0;j<n;j++)
+    {
+      cout<<n;
+    }
+    cout<<endl;
+  }
+}
