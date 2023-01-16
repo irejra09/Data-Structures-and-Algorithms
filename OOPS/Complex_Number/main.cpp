27508c47317f3cb1a49cc9398c69ab9584c012ef
Ishaan Rejra <ishaanrejra@gmail.com>
2023-01-16 23:12:34 +0530

    Update main.cpp

--- OOPS/Complex_Number/main.cpp
+++ OOPS/Complex_Number/main.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
 using namespace std;
-#include "Complex.cpp"
+class Complex {
+    // Complete this class
+    private:
+    int real;
+    int imaginary;
+
+    public:
+
+    Complex(int real,int imaginary)
+    {
+        this -> real = real;
+        this -> imaginary = imaginary;
+    }
+
+    void plus(Complex &C)
+    {
+        int rel = real + C.real;
+        int img = imaginary + C.imaginary;
+        real = rel;
+        imaginary = img;
+    }
+
+    void multiply(Complex &C)
+    {
+        int rel1 = (real* C.real) - (imaginary* (C.imaginary));
+        int img1 = (real * C.imaginary) + (imaginary* C.real);
+        real = rel1;
+        imaginary = img1;
+    }
+
+    void print()
+    {
+        cout<<real<<" + "<<"i"<<imaginary;
+    }
+    
+};
 
 int main() {
     int real1, imaginary1, real2, imaginary2;
